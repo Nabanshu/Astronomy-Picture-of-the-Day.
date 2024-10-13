3885ffeb1250b1c3cae91194c974ce52a1fdf1c0
Nabanshu Tewatia <124070793+Nabanshu@users.noreply.github.com>
2024-10-13 20:05:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import requests
 
 st.set_page_config(page_title="Astronomy Picture of the Day.",layout="wide",page_icon=":material/rocket_launch:")
 
-r = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={API_KEY}")
+r = requests.get(f"https://api.nasa.gov/planetary/apod?api_key={api}")
 content = r.json()
 
 col1, col2 = st.columns([0.6,0.5],vertical_alignment="center")
